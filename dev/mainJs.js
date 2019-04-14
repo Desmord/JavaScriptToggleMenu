ed65a0e80da57b129d38fe06afa96ff92b185ee4
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2019-04-14 13:38:36 +0200

    Project finished

--- dev/mainJs.js
+++ dev/mainJs.js
@@ -1,2 +1,45 @@
 import css from "./mainCss.css";
-console.log(`objffffect`);
+
+
+const content = document.querySelector(`.content`),
+    menuButton = document.querySelector(`.menuIcon`),
+    leftMargin = document.querySelector(`.menuMarginLeft`),
+    rightMargin = document.querySelector(`.menuMarginRight`),
+    menu = document.querySelector(`.menu`);
+
+let contentShow = true;
+
+const hideContent = () => content.style.opacity = `0`;
+
+const showContent = () => content.style.opacity = `1`;
+
+const showLeftMenuMargin = () => leftMargin.style.left = `0vw`;
+
+const hideLeftMenuMargin = () => leftMargin.style.left = `-25vw`;
+
+const showRightMenuMargin = () => rightMargin.style.left = `75vw`;
+
+const hideRightMenuMargin = () => rightMargin.style.left = `100vw`;
+
+const showMenu = () => menu.style.top = `0vh`;
+
+const hideMenu = () => menu.style.top = `-100vh`;
+
+
+menuButton.addEventListener(`click`, () => {
+
+    if (contentShow) {
+        hideContent();
+        contentShow = !contentShow;
+        showLeftMenuMargin();
+        showRightMenuMargin();
+        showMenu();
+    } else {
+        showContent();
+        contentShow = !contentShow;
+        hideLeftMenuMargin();
+        hideRightMenuMargin();
+        hideMenu();
+    }
+
+})
